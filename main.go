2a6afb10014ba18e87e2766e7075112a546f8f58
Danila Egorov <c4nsero@mail.ru>
2022-01-07 23:38:01 +0300

    + main.go

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"tgBot/mods"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	err := mods.InitConfig()
+	if err != nil {
+		log.Println("Config error: ", err)
+		return
+	}
+	botUrl := "https://api.telegram.org/bot" + viper.GetString("token")
+	offSet := 0
+	for {
+		updates, err := getUpdates(botUrl, offSet)
+		if err != nil {
+			log.Println("Something went wrong: ", err)
+		}
+		for _, update := range updates {
+			respond(botUrl, update)
+			offSet = update.UpdateId + 1
+		}
+		fmt.Println(updates)
+	}
+}
+
+func getUpdates(botUrl string, offset int) ([]mods.Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var restResponse mods.TelegramResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+	return restResponse.Result, nil
+}
+
+//	https://core.telegram.org/bots/api#using-a-local-bot-api-server
+func respond(botUrl string, update mods.Update) error {
+	msg := update.Message.Text
+
+	if msg == "/help" || msg == "/start" {
+		mods.SendMsg(botUrl, update, "/weather - узнать погоду")
+		return nil
+	}
+
+	switch msg {
+	case "/weather":
+		mods.SendMsg(botUrl, update, "workin'")
+		return nil
+	}
+
+	mods.SendMsg(botUrl, update, "I don't understand")
+	return nil
+}
